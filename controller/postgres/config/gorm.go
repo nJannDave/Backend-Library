3fa915a80305f3b1a2ea7b8c324824f6353751ea
bainn gallagher <paujanhapish@gmail.com>
2026-02-18 16:50:00 +0700

    feat: add cleanup func

--- controller/postgres/config/gorm.go
+++ controller/postgres/config/gorm.go
@@ -15,7 +15,7 @@ func ProviderConnStr() string {
 	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_NAME"), os.Getenv("POSTGRES_PORT"))
 }
 
-func Init(connStr string) *gorm.DB {
+func Init(connStr string) (*gorm.DB, func()) {
 	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Silent),
 	})
@@ -23,8 +23,13 @@ func Init(connStr string) *gorm.DB {
 		log.LogConfig("failed connect db", "connect_db_gorm", err)
 		panic(err)
 	}
+
 	sqlDB, _ := db.DB()
 	sqlDB.SetMaxIdleConns(5)
 	sqlDB.SetMaxOpenConns(20)
-	return db
+	return db, func(){
+		if err := sqlDB.Close(); err != nil {
+			panic(err)
+		}
+	}
 }
